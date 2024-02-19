e8339357254c736fee47965873e98188abfe89b7
Vincent Tieu <vincentktieu101@gmail.com>
2024-02-19 00:25:55 -0800

    fix single quotes that are preventing npm build

--- src/app/components/TimerDetails.tsx
+++ src/app/components/TimerDetails.tsx
@@ -5,16 +5,16 @@ export const TimerDetails = () => {
     <details className="p-2 mb-20 border-2 rounded-lg">
       <summary className="font-bold p-[-0.5rem]-">Purpose</summary>
       <div className="max-w-[500px] text-sm">
-        A timer that won't set off a loud alarm like{" "}
+        A timer that won&apos;t set off a loud alarm like{" "}
         <a
           href="https://www.google.com/search?q=timer"
           target="_blank"
           className="text-blue-400"
         >
-          Google's
+          Googl&apos;s
         </a>
         . The screen changes color when the time is reached. This timer might be
-        nice when you don't want to make noise or if your hands are busy.
+        nice when you don&apos;t want to make noise or if your hands are busy.
       </div>
     </details>
   );
